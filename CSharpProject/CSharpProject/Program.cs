b23724d024a4e33b2dd569a33b4e9903a705d647
Nluce <noluce@me.com>
2015-05-06 09:42:09 -0700

    5/2

---
+++ CSharpProject/CSharpProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    class Program
+    {
+        static string input; 
+        static void Main(string[] args)
+        {
+           
+                Console.Write("Enter your name\n");
+                input = Console.ReadLine();
+                Console.Write("Hello " + input + "\n");
+                while (true) ;
+        }
+    }
+}
